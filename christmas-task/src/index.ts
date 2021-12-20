04ae8a4ea94cc4eb1a245b12261ca3bd6998c370
BlackBerryID <iddf1994one@gmail.com>
2021-12-21 01:10:22 +0300

    docs: add console description

--- christmas-task/src/index.ts
+++ christmas-task/src/index.ts
@@ -3,6 +3,7 @@ import './components/noUiSlider/slider';
 import './components/general/setListeners';
 import { Cards } from './components/main/renderCards';
 import './components/main/filters';
+import './components/general/console';
 
 const cards = new Cards();
 cards.renderCards();
