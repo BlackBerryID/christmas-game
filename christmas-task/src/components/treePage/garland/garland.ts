7e7b6c6b7158e4107c33bdff862664b70b50a56a
BlackBerryID <iddf1994one@gmail.com>
2021-12-28 00:39:42 +0300

    feat: add garlandButtonsClicksHandler method and improve renderGarland method

--- christmas-task/src/components/treePage/garland/garland.ts
+++ christmas-task/src/components/treePage/garland/garland.ts
@@ -5,7 +5,7 @@ type IPixelRatioData = {
 const pixelRatioData: IPixelRatioData = {
   one: 120,
   two: 75,
-  three: 65,
+  three: 62,
   four: 55,
 };
 
@@ -13,23 +13,29 @@ class Garland {
   private treeWrapper: HTMLElement;
   private testBTN: HTMLElement;
   private garlandContainer: HTMLElement;
+  private garlandButtonsWrapper: HTMLElement;
 
   constructor() {
     this.treeWrapper = document.querySelector('.tree-column_wrapper') as HTMLElement;
     this.testBTN = document.querySelector('.blue-btn') as HTMLElement;
     this.garlandContainer = document.querySelector('.garland_container') as HTMLElement;
+    this.garlandButtonsWrapper = document.querySelector('.garland_buttons-wrapper') as HTMLElement;
   }
 
   addListener() {
-    // window.addEventListener('load', () => this.show());
-    this.testBTN.addEventListener('click', () => this.renderGarland());
+    this.garlandButtonsWrapper.addEventListener('click', (e) => this.garlandButtonsClicksHandler(e));
   }
 
-  show() {
-    console.log(this.treeWrapper.offsetWidth);
+  garlandButtonsClicksHandler(e: Event) {
+    if (!(e.target as HTMLElement).classList.contains('garland_btn')) return;
+    const color = (e.target as HTMLElement).dataset.color;
+    this.renderGarland(color!);
   }
 
-  renderGarland() {
+  renderGarland(color: string): void {
+    this.garlandContainer.className = `garland_container ${color}`;
+    const previousGarlandRopes = this.garlandContainer.querySelectorAll('ul');
+    previousGarlandRopes.forEach((item) => item.remove());
     this.garlandContainer.append(this.createRope('one'));
     this.garlandContainer.append(this.createRope('two'));
     this.garlandContainer.append(this.createRope('three'));
@@ -42,7 +48,9 @@ class Garland {
     const rope = document.createElement('ul');
     rope.classList.add('lightrope', lightrope);
     while (treeContainerWidth >= pixelsPerOneLight) {
-      rope.append(document.createElement('li'));
+      const li = document.createElement('li');
+      li.style.animationDuration = Math.random() * 1 + 1 + 's';
+      rope.append(li);
       treeContainerWidth = treeContainerWidth - pixelRatioData[lightrope];
     }
     return rope;
